f1a0bab92c002cc6f0dd27d91ecbe94697c16c26
dinhthi12 <thi884641@gmail.com>
2025-10-06 16:11:47 +0700

    Add p2p node(#2)

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,24 @@
-import { createConfig } from "./config/default"
-import { hashKey } from "./crypto/crypto"
-import { P2PNode } from "./network/p2pNode"
-import { saveChunk } from "./storage/fileManager"
-import { saveMetadata } from "./storage/metadata"
+import { createConfig } from './config/default'
+import { hashKey } from './crypto/crypto'
+import { P2PNode } from './network/p2pNode'
+import { saveChunk } from './storage/fileManager'
+import { saveMetadata } from './storage/metadata'
+import { logger } from './utils/logger'
 
 async function main() {
- const config = await createConfig()
+  const config = await createConfig()
 
   console.log('Node config:', config)
 
   const node = new P2PNode({ nodeId: config.nodeId, encryptionKey: config.encryptionKey }, config.storagePath)
   node.start(config.port)
 
-setTimeout(() => {
-    const data = Buffer.from("Hello from " + config.nodeId)
+  setTimeout(() => {
+    const data = Buffer.from('Hello from ' + config.nodeId)
     const chunk = data.toString('base64')
+
     const fileId = hashKey(chunk)
-    const fileName =  `hello-${config.nodeId}.txt`
+    const fileName = `image-${config.nodeId}.txt`
 
     saveChunk(config.storagePath, fileId, chunk)
     saveMetadata(config.storagePath, fileId, fileName)
@@ -25,9 +27,11 @@ setTimeout(() => {
       type: 'STORE',
       fileId,
       chunk,
-      fileName: `hello-${config.nodeId}.txt`,
+      fileName,
       from: config.nodeId
     })
+
+    logger.info(`[STORE] 📤 Broadcasted file ${fileName} (${fileId}) to peers`)
   }, 5000)
 
 }
